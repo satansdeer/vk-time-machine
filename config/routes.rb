15f182200067ff49412225f62a5db8feaa94a861
Maksim Ivanov <satansdeer@gmail.com>
2014-06-30 18:54:55 +0400

    [MASTER] added callback

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ VkTimeMachine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get 'oauth/callback'  => 'sessions#callback'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
