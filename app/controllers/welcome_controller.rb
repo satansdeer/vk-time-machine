464bf441d52de02fccee1cc4284a5f78a10b596d
Maksim Ivanov <satansdeer@gmail.com>
2014-06-30 19:10:38 +0400

    [MASTER] added user display

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,12 @@ class WelcomeController < ApplicationController
     session[:state] ||= Digest::MD5.hexdigest(rand.to_s)
     # и URL страницы авторизации
     @vk_url = VkontakteApi.authorization_url(scope: [:friends, :groups, :offline, :notify], state: session[:state])
+
+    if session[:token]
+      vk = VkontakteApi::Client.new(session[:token])
+
+      # текущий юзер
+      @user = vk.users.get(uid: session[:vk_id], fields: [:screen_name, :photo]).first
+    end  
   end
 end
